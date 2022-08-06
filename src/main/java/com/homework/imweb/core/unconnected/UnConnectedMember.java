db2704e4b9e607c44d657010d8afcb1de9314215
최선혁 <wafy75@gmail.com>
2022-08-06 14:51:32 +0900

    장기미사용 객체에 회원번호를 추가하라

--- src/main/java/com/homework/imweb/core/unconnected/UnConnectedMember.java
+++ src/main/java/com/homework/imweb/core/unconnected/UnConnectedMember.java
@@ -11,6 +11,8 @@ public class UnConnectedMember {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    private Long memberIdx;
+
     @Column(length = 100)
     private String email;
 
@@ -20,5 +22,28 @@ public class UnConnectedMember {
     private LocalDateTime joinDate;
 
     private LocalDateTime lastLoginTime;
+
+    public UnConnectedMember() {}
+
+
+    private UnConnectedMember(Long memberIdx,
+                              String email,
+                              String name,
+                              LocalDateTime joinDate,
+                              LocalDateTime lastLoginDate) {
+        this.memberIdx = memberIdx;
+        this.email = email;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.lastLoginTime = lastLoginDate;
+    }
+
+   public static UnConnectedMember of(Long memberIdx,
+                               String email,
+                               String name,
+                               LocalDateTime joinDate,
+                               LocalDateTime lastLoginDate) {
+        return new UnConnectedMember(memberIdx, email, name, joinDate, lastLoginDate);
+   }
 
 }
